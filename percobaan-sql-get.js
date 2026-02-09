c7bbbeddf0368c1a0666b452ba4b5f003a43ea4f
ajikowiyu <ajikowiyu@gmail.com>
2026-02-09 21:56:57 +0700

    node_modules & mysql2

---
+++ percobaan-sql-get.js
@@ -0,0 +1,36 @@
+// require punya 3 fungsi
+// 1. memanggil modul/fitur bawaan node.js
+// 2. memanggil modul/fitur dari npmjs
+// 3. memanggil file lain
+
+// panggil modul mysql2 dari node_modules
+const mysql = require('mysql2')
+
+// cara menghubungkan dengan database mysql
+const database = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'kelas_jfd'
+})
+
+database.connect()
+
+try {
+    database.query('SELECT * FROM karyawan', function(error, hasil) {
+        if (error) {
+            throw error.message()
+        }
+
+        let semuaNama = ''
+        for (const i in hasil) {
+            semuaNama += hasil[i].nama + '\n'
+        }
+
+        console.log(semuaNama);
+
+    })
+    database.end()
+} catch (error) {
+    console.log(error);
+}
